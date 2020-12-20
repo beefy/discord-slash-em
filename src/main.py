036fa3398f338639473eb4cbc646d5d18492e1ac
Nathaniel Schultz <nschultz13@bloomberg.net>
2020-12-19 23:51:09 -0500

    capital constants

--- src/main.py
+++ src/main.py
@@ -1,42 +1,42 @@
 import os
 import discord
 import paramiko
+from ansitoimg.render import ansiToRaster
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-slashem_user = os.getenv('USER')
-slashem_pass = os.getenv('PASS')
-discord_client = discord.Client()
-ssh_client = paramiko.SSHClient()
+SLASHEM_USER = os.getenv('USER')
+SLASHEM_PASS = os.getenv('PASS')
+DISCORD_CLIENT = discord.Client()
+SSH_CLIENT = paramiko.SSHClient()
 
 
-@discord_client.event
+@DISCORD_CLIENT.event
 async def on_ready() -> None:
-    print(f'{discord_client.user} is connected.\n')
+    print(f'{DISCORD_CLIENT.user} is connected.\n')
 
 
-@discord_client.event
+@DISCORD_CLIENT.event
 async def on_message(message: str) -> None:
     # ignore messages from ego
-    if message.author == discord_client.user:
+    if message.author == DISCORD_CLIENT.user:
         return
 
     # todo, parse message content and run discord cmd on ssh
     if message.content == 'slashem!':
         slashem_screen = call_ssh(message.content)
-        output = slashem_screen.encode('utf8').decode('latin-1')
-        await message.channel.send(output)
+        await message.channel.send(slashem_screen)
 
 
 def call_ssh(slashem_command: str) -> str:
     screen = "Something went wrong"
     try:
         print('connecting to ssh')
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname='alt.org', username='nethack', password='')
-        stdin, stdout, stderr = ssh_client.exec_command('')
+        SSH_CLIENT.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        SSH_CLIENT.connect(hostname='alt.org', username='nethack', password='')
+        stdin, stdout, stderr = SSH_CLIENT.exec_command('')
 
         print('logging into slashem account')
-        stdin.write(f'l{slashem_user}\n{slashem_pass}\npp')
+        stdin.write(f'l{SLASHEM_USER}\n{SLASHEM_PASS}\npp')
         stdin.flush()
 
         print('closing stdin and reading stdout')
@@ -46,11 +46,15 @@ def call_ssh(slashem_command: str) -> str:
 
     finally:
         print('ending ssh session')
-        ssh_client.close()
+        SSH_CLIENT.close()
 
+    output = screen.encode('utf8')
+    # ValueError: invalid literal for int() with base 10: '3;1H         By Stichting Mathe'
+    # ansiToRaster(output, "screen.png")
     return screen
 
 
 if __name__ == "__main__":
     # start the client
-    discord_client.run(TOKEN)
+    # DISCORD_CLIENT.run(TOKEN)
+    call_ssh('')
